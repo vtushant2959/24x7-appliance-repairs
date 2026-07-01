19878efcbc533ecaeda9f1475979654944103b95
vtushant2959 <vtushant2000@gmail.com>
2026-07-01 14:34:27 +0530

    Added contact form

--- lib/site.ts
+++ lib/site.ts
@@ -3,11 +3,11 @@ export const site = {
   url:
     process.env.NEXT_PUBLIC_SITE_URL ||
     "https://www.thehomeapplianceservices.com",
-  phone: "+91 98765 43210",
-  phoneHref: "tel:+919876543210",
+  phone: "+91 7669922991",
+  phoneHref: "tel:+917669922991",
   whatsappHref:
-    "https://wa.me/919876543210?text=I%20need%20home%20appliance%20repair%20service",
-  email: "care@thehomeapplianceservices.com",
+    "https://wa.me/917669922990?text=I%20need%20home%20appliance%20repair%20service",
+  email: "amirsaifi8882451030@gmail.com",
   hours: "24x7",
   address:
     "Doorstep appliance repair across Faridabad, South Delhi, Noida, Greater Noida, Gurugram, Ghaziabad and Ballabhgarh",
